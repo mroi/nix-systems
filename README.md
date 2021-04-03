65ea4e51c6abc8e3b69c6c1b0c6ad808ef372f72
Michael Roitzsch <reactorcontrol@icloud.com>
2021-04-03 17:23:10 +0200

    print-server: minimal SD card image

---
+++ README.md
@@ -0,0 +1,12 @@
+Systems Based on NixOS
+======================
+
+[NixOS](https://nixos.org) is a declarative approach for creating Linux-based systems. I am 
+using it to run these installations:
+
+* a little WiFi [print server](print-server) to avoid using the printer’s native (and likely 
+of sketchy security) WiFi functionality
+
+___
+This work is licensed under the [WTFPL](http://www.wtfpl.net/), so you can do anything you 
+want with it.
---
+++ print-server/default.nix
@@ -0,0 +1,17 @@
+let
+	configuration = { pkgs, ... }: {
+		# full cross compilation is possible, but too expensive
+		# nixpkgs.crossSystem = { config = "aarch64-linux"; };
+		imports = [
+			<nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
+			<nixpkgs/nixos/modules/profiles/minimal.nix>
+		];
+		sdImage.compressImage = false;
+	};
+
+	nixos = import <nixpkgs/nixos> {
+		inherit configuration;
+		system = "aarch64-linux";
+	};
+
+in nixos.config.system.build.sdImage
