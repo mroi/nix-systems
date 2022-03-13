9702cf93da3e09b629343597aedbc703a0e233ea
Michael Roitzsch <reactorcontrol@icloud.com>
2022-03-13 12:41:27 +0100

    organize image build using Nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+	description = "systems based on Nix and NixOS";
+	inputs.nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
+	outputs = { self, nixpkgs }: let
+		systems = [ "x86_64-linux"  "x86_64-darwin" ];
+		subdirs = [ "print-server" ];
+		lib = import "${nixpkgs}/lib";
+		forAll = list: f: lib.genAttrs list f;
+
+	in {
+		packages = forAll systems (system:
+			forAll subdirs (subdir:
+				import ./${subdir} { inherit nixpkgs system; }
+			)
+		);
+	};
+}
--- print-server/default.nix
+++ print-server/default.nix
@@ -1,15 +1,18 @@
+{ nixpkgs ? <nixpkgs>, system ? builtins.currentSystem }:
+with import nixpkgs { inherit system; };
+
 let
 	configuration = { pkgs, ... }: {
 		# full cross compilation is possible, but too expensive
 		# nixpkgs.crossSystem = { config = "aarch64-linux"; };
 		imports = [
-			<nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
-			<nixpkgs/nixos/modules/profiles/minimal.nix>
+			(nixpkgs + "/nixos/modules/installer/sd-card/sd-image-aarch64.nix")
+			(nixpkgs + "/nixos/modules/profiles/minimal.nix")
 		];
 		sdImage.compressImage = false;
 	};
 
-	nixos = import <nixpkgs/nixos> {
+	nixos = import (nixpkgs + "/nixos") {
 		inherit configuration;
 		system = "aarch64-linux";
 	};
