5c23c47e2d756e195b0193b976b74dfd11f6adb1
Michael Roitzsch <reactorcontrol@icloud.com>
2023-10-08 16:41:32 +0200

    print-server: install system configuration
    runs at first boot

--- print-server/default.nix
+++ print-server/default.nix
@@ -1,7 +1,7 @@
 { nixpkgs, system }:
 
 let
-	configuration = { modulesPath, ... }: {
+	configuration = { pkgs, modulesPath, ... }: {
 		imports = [
 			./configuration.nix
 			"${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
@@ -10,6 +10,32 @@ let
 			imageBaseName = builtins.baseNameOf ./.;
 			compressImage = false;
 		};
+		boot.postBootCommands =
+			let
+				folder = builtins.baseNameOf ./.;
+				configFiles = pkgs.stdenvNoCC.mkDerivation {
+					name = "configuration";
+					src = ./..;
+					phases = [ "unpackPhase" "installPhase" ];
+					installPhase = ''
+						mkdir $out
+						cp -r $src/${folder} $out/
+						cp $src/customization.nix $out/
+					'';
+				};
+			in ''
+				# install system configuration
+				if ! test -f /etc/nixos/configuration.nix ; then
+					cat > /etc/nixos/configuration.nix <<- 'EOF'
+						{ modulesPath, ... }: {
+							imports = [
+								${configFiles}/${folder}/configuration.nix
+								"''${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
+							];
+						}
+					EOF
+				fi
+			'';
 	};
 
 	nixos = nixpkgs.lib.nixosSystem {
