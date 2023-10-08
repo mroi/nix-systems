db50e4cb74e04947eb3d508c1e62b44570be36b9
Michael Roitzsch <reactorcontrol@icloud.com>
2023-10-08 16:41:32 +0200

    print-server: reduce SD card writes

--- print-server/configuration.nix
+++ print-server/configuration.nix
@@ -9,6 +9,16 @@
 	# FIXME: SSH crashes with the Rust nscd implementation
 	services.nscd.enableNsncd = false;
 
+	# reduce writes to the SD card and enable trimming
+	fileSystems."/".options = [ "noatime" ];
+	fileSystems."/tmp" = {
+		device = "none";
+		fsType = "tmpfs";
+		options = [ "mode=1777" ];
+	};
+	services.journald.extraConfig = "Storage=volatile";
+	services.fstrim.enable = true;
+
 	networking.hostName = "nixos-${builtins.baseNameOf ./.}";
 	networking.firewall.enable = false;
 	services.avahi.enable = true;
@@ -18,7 +28,6 @@
 		settings.PasswordAuthentication = false;
 		settings.KbdInteractiveAuthentication = false;
 	};
-
 	users.users.root.openssh.authorizedKeys.keys = [
 		config.customization.authorizedKey
 	];
