ed5383d28c26630f248c39454c59211047ee4ebd
Michael Roitzsch <reactorcontrol@icloud.com>
2023-12-21 09:14:46 +0100

    vmware-host: add user separation
    patch sunshine to track user requests based on the presented client
    certificate

--- vmware-host/sunshine-overlay/default.nix
+++ vmware-host/sunshine-overlay/default.nix
@@ -5,6 +5,10 @@ self: super: {
 			# libpulse restricts the permissions of the pulse socket directory
 			# we need the socket accessible by other users, so we add g+rx
 			./pulse-permissions.patch
+			# use the client certificate to differentiate apps by user:
+			# the sunshine state file is extended with a username field
+			# the sunshine apps file is extended with a per-app user-filter field
+			./user-separation.patch
 		];
 	});
 }
