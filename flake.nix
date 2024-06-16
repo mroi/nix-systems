704d11ed2aef7be0733976088a414db37644de97
Michael Roitzsch <reactorcontrol@icloud.com>
2024-06-16 15:17:31 +0200

    time-machine: initial configuration

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 	inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 	outputs = { self, nixpkgs }: let
 		systems = [ "x86_64-linux"  "x86_64-darwin" ];
-		subdirs = [ "print-server" "rescue" ];
+		subdirs = [ "print-server" "rescue" "time-machine" ];
 		modules = [ "auto-upgrade" "config-install" "conserve-storage" "ssh-wifi-access" "stress-test-tools" ];
 		forAll = list: f: nixpkgs.lib.genAttrs list f;
 		callPackage = system: nixpkgs.lib.callPackageWith (nixpkgs.legacyPackages.${system} // {
---
+++ time-machine/configuration.nix
@@ -0,0 +1,16 @@
+{ lib, modulesPath, ... }: {
+	imports = [
+		"${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
+		"${modulesPath}/profiles/headless.nix"
+		../modules/auto-upgrade.nix
+		../modules/conserve-storage.nix
+		../modules/ssh-wifi-access.nix
+		../customization.nix
+	];
+	system.stateVersion = "24.11";
+	nixpkgs.system = "aarch64-linux";
+
+	# disable WiFi, Ethernet only
+	networking.hostName = "chaldene";
+	networking.wireless.enable = lib.mkForce false;
+}
