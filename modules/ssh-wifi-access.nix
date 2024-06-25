1e505cc1e0c4240f4577fe854260928d9f64c977
Michael Roitzsch <reactorcontrol@icloud.com>
2024-06-25 09:30:36 +0200

    modules: capitalize Avahi SSH advertisement

--- modules/ssh-wifi-access.nix
+++ modules/ssh-wifi-access.nix
@@ -1,17 +1,9 @@
-{ config, pkgs, ... }: {
+{ config, lib, ... }: {
 
 	# configure basic network services
 	networking = {
 		wireless.enable = true;
 		wireless.networks.${config.customization.wifi.ssid}.psk = config.customization.wifi.password;
-	};
-	services.avahi = {
-		enable = true;
-		publish.enable = true;
-		publish.userServices = true;
-		extraServiceFiles = {
-			ssh = "${pkgs.avahi}/etc/avahi/services/ssh.service";
-		};
 	};
 	services.openssh = {
 		enable = true;
@@ -22,4 +14,23 @@
 	users.users.root.openssh.authorizedKeys.keys = [
 		config.customization.authorizedKey
 	];
+	services.avahi = {
+		enable = true;
+		publish.enable = true;
+		publish.userServices = true;
+		extraServiceFiles.ssh = let
+			capitalizedHostName =
+				(lib.toUpper (lib.substring 0 1 config.networking.hostName)) +
+				(lib.toLower (lib.substring 1 (-1) config.networking.hostName));
+		in ''<?xml version="1.0" standalone='no'?>
+			<!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+			<service-group>
+				<name>${capitalizedHostName}</name>
+				<service>
+					<type>_ssh._tcp</type>
+					<port>22</port>
+				</service>
+			</service-group>
+		'';
+	};
 }
