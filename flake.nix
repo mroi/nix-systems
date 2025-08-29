0ea60c7119ce832b30f18ded452c1ed5bda0906c
Michael Roitzsch <reactorcontrol@icloud.com>
2025-08-30 00:30:49 +0200

    vmware-host: filter incompatible QEMU VM apps

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
 		callPackage = system: nixpkgs.lib.callPackageWith (nixpkgs.legacyPackages.${system} // {
 			inherit (nixpkgs) lib;
 		});
+		unsupported = {
+			"aarch64-darwin" = [ "vmware-host" ];
+			"aarch64-linux" = [ "vmware-host" ];
+		};
 
 	in {
 		packages = forAll systems (system:
@@ -16,43 +20,45 @@
 				callPackage system ./${subdir}/package.nix {}
 			)
 		);
-		apps = forAll systems (system:
-			forAll subdirs (subdir:
-				let
-					configuration = { lib, modulesPath, ... }: {
-						imports = [
-							./${subdir}/configuration.nix
-							"${modulesPath}/virtualisation/qemu-vm.nix"
-						];
-						nixpkgs.hostPlatform = lib.mkForce
-							(builtins.replaceStrings [ "darwin" ] [ "linux" ] system);
-						virtualisation.host.pkgs = nixpkgs.legacyPackages.${system};
-						virtualisation.qemu.guestAgent.enable = false;
-						virtualisation.forwardPorts = [{
-							from = "host";
-							host.address = "127.0.0.1";
-							host.port = 22022;
-							guest.port = 22;
-						}];
+		apps = builtins.mapAttrs (n: v: builtins.removeAttrs v unsupported."${n}" or []) (
+			forAll systems (system:
+				forAll subdirs (subdir:
+					let
+						configuration = { lib, modulesPath, ... }: {
+							imports = [
+								./${subdir}/configuration.nix
+								"${modulesPath}/virtualisation/qemu-vm.nix"
+							];
+							nixpkgs.hostPlatform = lib.mkForce
+								(builtins.replaceStrings [ "darwin" ] [ "linux" ] system);
+							virtualisation.host.pkgs = nixpkgs.legacyPackages.${system};
+							virtualisation.qemu.guestAgent.enable = false;
+							virtualisation.forwardPorts = [{
+								from = "host";
+								host.address = "127.0.0.1";
+								host.port = 22022;
+								guest.port = 22;
+							}];
+						};
+						nixos = nixpkgs.lib.nixosSystem {
+							modules = [ configuration ];
+							specialArgs = { name = subdir; };
+						};
+					in {
+						type = "app";
+						program = "${nixos.config.system.build.vm}/bin/run-${nixos.config.networking.hostName}-vm";
+						meta.description = "QEMU VM of ${subdir} for testing";
+					}
+				) // {
+					ssh = {
+						type = "app";
+						program = "${nixpkgs.legacyPackages.${system}.writeScript "ssh-nixos" ''
+							ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i ~/.ssh/nixos -p 22022 root@localhost
+						''}";
+						meta.description = "SSH client connecting to a test VM";
 					};
-					nixos = nixpkgs.lib.nixosSystem {
-						modules = [ configuration ];
-						specialArgs = { name = subdir; };
-					};
-				in {
-					type = "app";
-					program = "${nixos.config.system.build.vm}/bin/run-${nixos.config.networking.hostName}-vm";
-					meta.description = "QEMU VM of ${subdir} for testing";
 				}
-			) // {
-				ssh = {
-					type = "app";
-					program = "${nixpkgs.legacyPackages.${system}.writeScript "ssh-nixos" ''
-						ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i ~/.ssh/nixos -p 22022 root@localhost
-					''}";
-					meta.description = "SSH client connecting to a test VM";
-				};
-			}
+			)
 		);
 		nixosModules = forAll modules (module:
 			import modules/${module}.nix
