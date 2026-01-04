ac2c11adf66635fddf3600aebc05528ed40a6092
Michael Roitzsch <reactorcontrol@icloud.com>
2026-01-04 10:58:48 +0100

    flake: change to official spelling of NixOS

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
 	description = "systems based on Nix and NixOS";
-	inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 	outputs = { self, nixpkgs }: let
 		systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux"  "x86_64-darwin" ];
 		subdirs = [ "print-server" "rescue" "time-machine" ];
